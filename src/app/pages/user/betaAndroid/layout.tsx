957a09cfa85a1ebc0432c074944bffb9e39f8649
istoriya-islama <istoriyaislama@hotmail.com>
2025-08-06 19:17:23 +0300

    Element four

---
+++ src/app/pages/user/betaAndroid/layout.tsx
@@ -0,0 +1,9 @@
+import type { PropsWithChildren } from 'react'
+
+export default function Layout({ children }: PropsWithChildren<unknown>) {
+  return (
+    <div className='w-full bg-gradient-to-r from-blue-500 via-black to-blue-800 animate-gradient-x pb-2'>
+      {children}
+    </div>
+  )
+}
--- src/app/pages/user/layout.tsx
+++ src/app/pages/user/layout.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react'
 
 export default function Layout({ children }: PropsWithChildren<unknown>) {
   return (
-    <div className='w-full bg-gradient-to-r from-blue-500 via-black to-blue-800 animate-gradient-x pb-2'>
+    <div className='w-full bg-gradient-to-r from-blue-500 via-black to-blue-800 animate-gradient-x pb-2 h-200'>
       {children}
     </div>
   )
