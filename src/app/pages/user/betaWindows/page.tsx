c1949b04cd1b8e2ae9a325c66194ccb3b336d4a3
istoriya-islama <istoriyaislama@hotmail.com>
2025-09-24 15:13:42 +0300

    Error for page Beta for Windows

--- src/app/pages/user/betaWindows/page.tsx
+++ src/app/pages/user/betaWindows/page.tsx
@@ -135,6 +135,8 @@ export default function UserPage() {
               Скачать
             </a>
           </div>
+        </div>
+        <div className='flex justify-center items-center gap-4 mt-8'>
           <div className='bg-blue-950/20 backdrop-blur-3xl rounded-4xl p-5 shadow-2xl shadow-blue-500'>
             <img src='/weather.ico' alt='OREL Погода' />
             <h1 className='text-xl font-bold'>OREL Weather 2.3</h1>
