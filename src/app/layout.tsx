3d50c1976aa0c42a05f4bdaabd5561dd0a9ce1bf
istoriya-islama <istoriyaislama@hotmail.com>
2025-10-08 14:58:28 +0300

    Rename font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Alegreya } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const alegreya = Alegreya({
+  subsets: ['latin'], // обязательно укажи
+  variable: '--font-alegreya' // если нужны разные толщины
 })
 
 export const metadata: Metadata = {
@@ -17,17 +12,14 @@ export const metadata: Metadata = {
   description: 'OREL Insider for developer',
 }
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en">
+      <body className={`${alegreya.variable} antialiased`}>
         {children}
       </body>
     </html>
